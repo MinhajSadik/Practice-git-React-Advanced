6c14934cf06413ac1e846203a589b62628bf6303
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-20 19:10:32 +0600

    [add] class component componentDidMount and componentDidUpdate practice code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import HooksArray from './Components/HooksArray/HooksArray';
+// import HooksArray from './Components/HooksArray/HooksArray';
 // import ClassCounter from './Components/Counter/ClassCounter';
 // import Counter from './Components/Counter/Counter';
 // import HookObject from './Components/HooksObject/HooksObject';
+import UseEffectHook from './Components/UseEffectHook/ClassHook';
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       {/* <Counter/>
       <ClassCounter/> */}
       {/* <HookObject/> */}
-      <HooksArray/>
+      {/* <HooksArray/> */}
+      <UseEffectHook/>
     </div>
   );
 }
