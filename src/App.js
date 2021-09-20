0c62e494d61ce87e015d10ac6973621cc1e12fb6
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-20 13:08:09 +0600

    [add] hookObject part implement and practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import ClassCounter from './Components/Counter/ClassCounter';
-import Counter from './Components/Counter/Counter';
+// import ClassCounter from './Components/Counter/ClassCounter';
+// import Counter from './Components/Counter/Counter';
+import HookObject from './Components/HookObject/HookObject';
 
 const App = () => {
   return (
     <div className="App">
-      <Counter/>
-      <ClassCounter/>
+      {/* <Counter/>
+      <ClassCounter/> */}
+      <HookObject/>
     </div>
   );
 }
